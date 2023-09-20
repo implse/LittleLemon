de172797ea46aecba470ed2f5fad5af170d5053d
Finite <finite.impulse@gmail.com>
2023-09-20 07:44:02 +0200

    Add serializers, views and routers

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Booking
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.SmallIntegerField()
@@ -9,6 +9,7 @@ class Booking(models.Model):
     def __str__(self):
         return self.name
 
+# Menu
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=10)
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+# API: Menu
+class MenuItemView(ListCreateAPIView):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
+
+# API: Single Menu
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
+
+# API: Booking
+class BookingViewSet(ModelViewSet):
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
+
+
