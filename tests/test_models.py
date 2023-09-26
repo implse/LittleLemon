6c66622c4de3659bfc141b5992c8c8062cb047b4
Finite <finite.impulse@gmail.com>
2023-09-26 08:08:01 +0200

    Improve models and views tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,26 @@ from django.test import TestCase
 from restaurant.models import Menu
 
 class MenuTest(TestCase):
+
+    def setUp(self):
+        Menu.objects.create(title="Salade caesar", price=12)
+        Menu.objects.create(title="Millefeuille végé", price=10)
+        Menu.objects.create(title="Suprême de volaille", price=14)
+        Menu.objects.create(title="Macoha burger", price=17)
+
     def test_get_menu(self):
-        item = Menu.objects.create(title="Salade caesar", price=12)
-        self.assertEqual(item.__str__(), "Salade caesar : 12")
+        menu1 = Menu.objects.get(title="Salade caesar")
+        menu2 = Menu.objects.get(title="Millefeuille végé")
+        menu3 = Menu.objects.get(title="Suprême de volaille")
+        menu4 = Menu.objects.get(title="Macoha burger")
+        self.assertEqual(f'{menu1.title} : {menu1.price}', "Salade caesar : 12.00")
+        self.assertEqual(f'{menu2.title} : {menu2.price}', "Millefeuille végé : 10.00")
+        self.assertEqual(f'{menu3.title} : {menu3.price}', "Suprême de volaille : 14.00")
+        self.assertEqual(f'{menu4.title} : {menu4.price}', "Macoha burger : 17.00")
+
+    def test_update_menu(self):
+        menu_burger = Menu.objects.get(title="Macoha burger")
+        menu_burger.price = 15
+        menu_burger.save()
+        update_menu = Menu.objects.get(title="Macoha burger")
+        self.assertEqual(f'{update_menu.title} : {update_menu.price}', "Macoha burger : 15.00")
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,21 +1,40 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient, APITestCase
 from restaurant.models import Menu
 from restaurant.serializers import MenuSerializer
 from rest_framework import status
 from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
-class MenuViewTest(TestCase):
+class MenuViewTest(APITestCase):
+
     def setUp(self):
+        self.user = User.objects.create_user(username='citron', password='lemon@123!')
+        self.token = Token.objects.create(user=self.user)
         Menu.objects.create(title="Salade caesar", price=12)
         Menu.objects.create(title="Millefeuille végé", price=10)
-        Menu.objects.create(title="MSuprême de volaille", price=14)
-        Menu.objects.create(title="Macoha Burger", price=17)
+        Menu.objects.create(title="Suprême de volaille", price=14)
+        Menu.objects.create(title="Macoha burger", price=17)
 
     def test_get_all(self):
         client = APIClient()
-        response = client.get('/restaurant/menu/', format='json')
+        client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        response = client.get(f'/restaurant/menu/', format='json')
         menus = Menu.objects.all()
         serializer = MenuSerializer(menus, many=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
+
+    def test_get_single(self):
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        menu = Menu.objects.get(title="Salade caesar")
+        response = client.get(f'/restaurant/menu/{menu.id}', format='json')
+        serializer = MenuSerializer(menu)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+
+
 
